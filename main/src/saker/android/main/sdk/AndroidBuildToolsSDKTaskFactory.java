cb6c436e4c76fab7ba2d42dd9773cf6c16a202a2
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-02-24 13:51:06 +0100

    SDK configuration task factories

---
+++ main/src/saker/android/main/sdk/AndroidBuildToolsSDKTaskFactory.java
@@ -0,0 +1,40 @@
+package saker.android.main.sdk;
+
+import java.util.Collection;
+import java.util.Set;
+
+import saker.android.impl.sdk.VersionsAndroidBuildToolsSDKDescription;
+import saker.build.runtime.execution.ExecutionContext;
+import saker.build.task.ParameterizableTask;
+import saker.build.task.TaskContext;
+import saker.build.task.utils.annot.SakerInput;
+import saker.build.thirdparty.saker.util.ImmutableUtils;
+import saker.build.trace.BuildTrace;
+import saker.nest.utils.FrontendTaskFactory;
+import saker.sdk.support.api.SDKDescription;
+
+public class AndroidBuildToolsSDKTaskFactory extends FrontendTaskFactory<SDKDescription> {
+	private static final long serialVersionUID = 1L;
+
+	public static final String TASK_NAME = "saker.android.sdk.buildtools";
+
+	@Override
+	public ParameterizableTask<? extends SDKDescription> createTask(ExecutionContext executioncontext) {
+		return new ParameterizableTask<SDKDescription>() {
+
+			@SakerInput(value = { "", "Version", "Versions" })
+			public Collection<String> versionsOption;
+
+			@Override
+			public SDKDescription run(TaskContext taskcontext) throws Exception {
+				if (saker.build.meta.Versions.VERSION_FULL_COMPOUND >= 8_006) {
+					BuildTrace.classifyTask(BuildTrace.CLASSIFICATION_CONFIGURATION);
+				}
+
+				Set<String> versions = ImmutableUtils.makeImmutableNavigableSet(versionsOption);
+				return VersionsAndroidBuildToolsSDKDescription.create(versions);
+			}
+		};
+	}
+
+}
