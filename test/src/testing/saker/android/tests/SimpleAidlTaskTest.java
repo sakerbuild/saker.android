795858e16f08cd6c41410614c7cff160ca009017
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-03-10 20:41:32 +0100

    Simple aidl test

---
+++ test/src/testing/saker/android/tests/SimpleAidlTaskTest.java
@@ -0,0 +1,34 @@
+package testing.saker.android.tests;
+
+import saker.build.file.path.SakerPath;
+import saker.build.thirdparty.saker.util.io.ByteArrayRegion;
+import testing.saker.SakerTest;
+
+@SakerTest
+public class SimpleAidlTaskTest extends BaseAndroidTestCase {
+	//example Aidl taken from https://developer.android.com/guide/components/aidl
+
+	@Override
+	protected void runNestTaskTestImpl() throws Throwable {
+		touchLocalFileForIncrementalTesting(getMirrorDirectory()
+				.resolve("bd_/saker.android.aidl/aidl/java/com/example/android/IRemoteService.java"));
+		SakerPath javaoutpath = PATH_BUILD_DIRECTORY
+				.resolve("saker.android.aidl/aidl/java/com/example/android/IRemoteService.java");
+
+		runScriptTask("build");
+		ByteArrayRegion bytes1 = files.getAllBytes(javaoutpath);
+
+		runScriptTask("build");
+		assertEmpty(getMetric().getRunTaskIdResults());
+
+		SakerPath aidlpath = PATH_WORKING_DIRECTORY.resolve("aidl/com/example/android/IRemoteService.aidl");
+		files.putFile(aidlpath, files.getAllBytes(aidlpath).toString().replace("int anInt", "long anInt"));
+
+		runScriptTask("build");
+		assertNotEmpty(getMetric().getRunTaskIdResults());
+		ByteArrayRegion bytes2 = files.getAllBytes(javaoutpath);
+		//assert that the output file changed
+		assertFalse(bytes1.regionEquals(bytes2));
+	}
+
+}
--- test/src/testing/saker/android/tests/SimpleD8TaskTest.java
+++ test/src/testing/saker/android/tests/SimpleD8TaskTest.java
@@ -11,8 +11,8 @@ public class SimpleD8TaskTest extends BaseAndroidTestCase {
 	protected void runNestTaskTestImpl() throws Throwable {
 		runScriptTask("build");
 		//check existence
-		ByteArrayRegion bytes1 = files
-				.getAllBytes(PATH_BUILD_DIRECTORY.resolve("saker.android.d8/src/classes/classes.dex"));
+		SakerPath classesdexoutpath = PATH_BUILD_DIRECTORY.resolve("saker.android.d8/src/classes/classes.dex");
+		ByteArrayRegion bytes1 = files.getAllBytes(classesdexoutpath);
 
 		runScriptTask("build");
 		assertEmpty(getMetric().getRunTaskIdResults());
@@ -20,8 +20,7 @@ public class SimpleD8TaskTest extends BaseAndroidTestCase {
 		SakerPath mainjavapath = PATH_WORKING_DIRECTORY.resolve("src/test/Main.java");
 		files.putFile(mainjavapath, files.getAllBytes(mainjavapath).toString().replace("//f", "//f\npublic int i;"));
 		runScriptTask("build");
-		ByteArrayRegion bytes2 = files
-				.getAllBytes(PATH_BUILD_DIRECTORY.resolve("saker.android.d8/src/classes/classes.dex"));
+		ByteArrayRegion bytes2 = files.getAllBytes(classesdexoutpath);
 		//assert that the output dex file changed
 		assertFalse(bytes1.regionEquals(bytes2));
 	}
