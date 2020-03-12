e4a9f299a43140efe863c2f570d5b2e61f6a28d3
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-03-12 20:10:48 +0100

    Simple aapt2 link task test

---
+++ test/src/testing/saker/android/tests/SimpleAAPT2LinkTaskTest.java
@@ -0,0 +1,36 @@
+package testing.saker.android.tests;
+
+import saker.build.file.path.SakerPath;
+import saker.build.thirdparty.saker.util.io.ByteArrayRegion;
+import testing.saker.SakerTest;
+
+@SakerTest
+public class SimpleAAPT2LinkTaskTest extends BaseAndroidTestCase {
+
+	@Override
+	protected void runNestTaskTestImpl() throws Throwable {
+		SakerPath apkoutpath = PATH_BUILD_DIRECTORY.resolve("saker.android.aapt2.link/default/output.apk");
+		SakerPath strxmlpath = PATH_WORKING_DIRECTORY.resolve("res/values/strings.xml");
+		SakerPath manifestpath = PATH_WORKING_DIRECTORY.resolve("AndroidManifest.xml");
+
+		runScriptTask("build");
+		ByteArrayRegion bytes1 = files.getAllBytes(apkoutpath);
+		//test R.java existence
+		files.getFileAttributes(PATH_BUILD_DIRECTORY.resolve("saker.android.aapt2.link/default/java/com/example/R.java"));
+
+		runScriptTask("build");
+		assertEmpty(getMetric().getRunTaskIdResults());
+
+		files.putFile(strxmlpath, files.getAllBytes(strxmlpath).toString().replace("Example", "Modified"));
+		runScriptTask("build");
+		ByteArrayRegion bytes2 = files.getAllBytes(apkoutpath);
+		//assert that the output file changed
+		assertFalse(bytes1.regionEquals(bytes2));
+
+		files.putFile(manifestpath, files.getAllBytes(manifestpath).toString().replace("21", "23"));
+		runScriptTask("build");
+		ByteArrayRegion bytes3 = files.getAllBytes(apkoutpath);
+		assertFalse(bytes2.regionEquals(bytes3));
+	}
+
+}
